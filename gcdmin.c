e04ef516a0a5266ba6870e5e7abcfc32f54ed3c5
dhrumilpy154-hue <dhrumilpy154@gmail.com>
2025-12-04 20:33:03 +0530

    Add GCD and LCM calculation in gcdmin.c

---
+++ gcdmin.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int min(int a,int b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+int gcd(int a,int b){
+    int hcf;
+    for(int i=1;i<=min(a,b);i++){
+        if(a%i==0 && b%i==0){
+            hcf = i;
+        }
+        
+    }
+    return hcf;
+}
+int main(){
+
+    int a,b;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+
+    int hcf = gcd(a,b);
+    int lcm = a*b/gcd(a,b);
+
+    printf("The HCf of number %d and %d is %d\n",a,b,hcf);
+    printf("The LCM of number %d and %d is %d",a,b,lcm);
+
+    return 0;
+}
